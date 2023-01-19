14b946d6a2dcfef5ffa2ef526245158110c7b267
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-01-19 12:32:13 -0300

    final timeline

--- src/components/timeline/index.vue
+++ src/components/timeline/index.vue
@@ -17,15 +17,16 @@
 <style lang="scss" scoped>
 .timeline {
   width: 300px;
+  position: relative;
 
   &__top {
-    width: 100%;
+    width: 300px;
     height: 60px;
     display: flex;
     justify-content: center;
-    border: 1px solid red;
+    align-items: center;
     &__line {
-      width: 90%;
+      width: 200px;
       height: 4px;
       background: blue;
       border-radius: 4px;
@@ -37,31 +38,39 @@
     height: 60px;
     display: flex;
     justify-content: space-around;
+    align-items: center;
     border: 1px solid red;
-
+    position: absolute;
+    bottom: 2%;
     &__one {
-      width: 40px;
-      height: 40px;
+      width: 20px;
+      height: 20px;
       border-radius: 50%;
       display: flex;
       justify-content: center;
       align-items: center;
+      border: 1px solid blue;
+      background: #fff;
     }
     &__two {
-      width: 40px;
-      height: 40px;
+      width: 20px;
+      height: 20px;
       border-radius: 50%;
       display: flex;
       justify-content: center;
       align-items: center;
+      border: 1px solid blue;
+      background: #fff;
     }
     &__three {
-      width: 40px;
-      height: 40px;
+      width: 20px;
+      height: 20px;
       border-radius: 50%;
       display: flex;
       justify-content: center;
       align-items: center;
+      border: 1px solid blue;
+      background: #fff;
     }
   }
 }
